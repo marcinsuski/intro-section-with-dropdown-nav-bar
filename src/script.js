9e83637991727f751666288715ecb8cb060eae59
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2022-11-09 15:26:05 +0100

    final works
    - added javascript for submenu
    - added styling for submenu
    - added styling for hover status

--- src/script.js
+++ src/script.js
@@ -1,13 +1,40 @@
-const mobNav = document.querySelector(".burger-nav");
+//variables
+
+const mobiNavi = document.querySelector(".burger-nav");
 const burgerBtn = document.querySelector(".burger");
 const openMenu = document.querySelector(".open");
 const closeMenu = document.querySelector(".close");
+const modal = document.querySelector(".modal");
+const features = document.querySelector(".features");
+const company = document.querySelector(".company");
+const featuresSubmenu = document.querySelector(".submenu1");
+const companySubmenu = document.querySelector(".submenu2");
 
+//burger button on mobile view
 function toggleMenu() {
-    mobNav.classList.toggle("active");
+    mobiNavi.classList.toggle("active");
     burgerBtn.classList.toggle("active");
+
     openMenu.classList.toggle("hidden");
+
     closeMenu.classList.toggle("hidden");
+    modal.classList.toggle("hidden");
 }
 
+// submenu
+
+function featuresOpen() {
+    features.classList.toggle("open");
+    featuresSubmenu.classList.toggle("collapse");
+}
+
+function companyOpen() {
+    company.classList.toggle("open");
+    companySubmenu.classList.toggle("collapse");
+}
+
+// Listeners
+
 burgerBtn.addEventListener("click", toggleMenu);
+features.addEventListener("click", featuresOpen);
+company.addEventListener("click", companyOpen);
